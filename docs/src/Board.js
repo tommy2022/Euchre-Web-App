25e482250af61de9b497155bd928f289cd220769
joyhe <joyhe@umich.edu>
2022-08-08 13:40:00 -0400

    ui boxes created excluding player 1

--- docs/src/Board.js
+++ docs/src/Board.js
@@ -3,6 +3,7 @@ import { CgGhostCharacter } from "react-icons/cg";
 import { IoLogoOctocat } from "react-icons/io";
 import { SiDatadog } from "react-icons/si";
 import { GiHumanPyramid } from "react-icons/gi";
+import { Grid, GridItem, Center } from '@chakra-ui/react'
 
 import Player from "./Components/Player";
 import PlayerInfo from "./Components/PlayerInfo";
@@ -28,18 +29,43 @@ function Board() {
     human: GiHumanPyramid,
   };
   return (
-    <div className="Board">
-      Hello World
-      <Player pfp={pfps.human} tricksWon={2} />
-      <PlayerInfo name="Kitty" tricksWon={1} pfp={pfps.cat} team="red" />
-      <PlayerInfo name="Doggo" tricksWon={2} pfp={pfps.dog} team="blue" />
-      <PlayerInfo name="King Boo" tricksWon={1} pfp={pfps.ghost} team="red" />
-      <br />
-      <Card suit={HEARTS} rank={QUEEN} />
-      <Card suit={SPADES} rank={ACE} />
-      <Card suit={DIAMONDS} rank={KING} />
-      <Card suit={CLUBS} rank={JACK} />
-    </div>
+    <Grid
+      gap={10}
+      templateColumns='repeat(3, 1fr)'
+    >
+      <GridItem w='100%' h='150' bg='tomato'>
+        Blank
+      </GridItem>
+      <GridItem w='100%' h='150' bg='papayawhip'>
+        <PlayerInfo name="Kitty" tricksWon={1} pfp={pfps.cat} team="red" />
+      </GridItem>
+      <GridItem w='100%' h='150' bg='tomato'>
+        Trump
+      </GridItem>
+      <GridItem w='100%' h='300' bg='papayawhip'>
+        P2
+      </GridItem>
+      <GridItem w='100%' h='300' bg='tomato'>
+        Cards
+      </GridItem>
+      <GridItem w='100%' h='300' bg='papayawhip'>
+        P4
+      </GridItem>
+    </Grid>
+
+
+    // <div className="Board">
+    //   Hello World
+    //   <Player pfp={pfps.human} tricksWon={2} />
+    //   <PlayerInfo name="Kitty" tricksWon={1} pfp={pfps.cat} team="red" />
+    //   <PlayerInfo name="Doggo" tricksWon={2} pfp={pfps.dog} team="blue" />
+    //   <PlayerInfo name="King Boo" tricksWon={1} pfp={pfps.ghost} team="red" />
+    //   <br />
+    //   <Card suit={HEARTS} rank={QUEEN} />
+    //   <Card suit={SPADES} rank={ACE} />
+    //   <Card suit={DIAMONDS} rank={KING} />
+    //   <Card suit={CLUBS} rank={JACK} />
+    // </div>
   );
 }
 
